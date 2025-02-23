3553c4670676dfc4ffa972c6e597cc2fe3caa709
Chocoo231 <chocoo090916@gmail.com>
2025-02-23 17:12:43 +0900

    Create Stock.py

---
+++ Stock.py
@@ -0,0 +1,25 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+def Stock_main():
+    st = Toplevel()
+
+    img =Image.open('./A.gif')
+    bg = ImageTk.PhotoImage(img)
+
+    win.geometry("650x450")
+
+    # 배경을 Label을 이용하여 처리
+    label = Label(st, image=bg)
+    label.place(x = -2,y = -2)
+
+    button_gambling = Button(st, text='도박', fg='white', bg='black', font=15, width=15, height=3, command=Gambling)
+
+    # Tkinter 수행
+    win.mainloop()
+
+     
+
+    # 배경 이미지의 좌표를 0, 0 으로 하면 약간의 빈 공간이 나타난다.
+
+    label.place(x =0,y =0)
